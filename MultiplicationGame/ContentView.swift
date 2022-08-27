dade2cb0b0f0b88e5e87fadff65f1254bc579b93
Marc Schwartz <marcschwartz1990@gmail.com>
2022-08-27 16:03:00 -0400

    Initial Commit

---
+++ MultiplicationGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationGame
+//
+//  Created by Marc-Developer on 8/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Marc-Developer on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
